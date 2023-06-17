6d612185ebed0bb48aa66484520bccc5f64c885c
zinq7 <87585065+zinq7@users.noreply.github.com>
2023-06-17 14:38:03 -0400

    add body portraits

---
+++ lib/stage_helper.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+List getStageEvent(List json) {
+  List lst = json;
+  int start = 0, end = lst.indexWhere((element) => element["eventType"] == "StageEndEvent");
+
+  if (end == -1) return [json, []]; // no stageEnd is last stage
+
+  return [lst.sublist(start, end + 1), lst.sublist(end + 1)]; // separate on end
+}
+
+List getStageEvents(String jsonString) {
+  // read run
+  var json = jsonDecode(jsonString);
+
+  // get events (to start)
+  List events = getStageEvent(json["runEvents"]);
+  List stageEvents = [];
+  int stage = 1;
+
+  // loop through and fill list with stages
+  while (!events[1].isEmpty) {
+    stageEvents.add(events[0]); // events[0] is the stage
+
+    events = getStageEvent(events[1]); // events[1] is the rest
+    stage++;
+
+    if (stage > 6) break; // unlikely, but ignore loops
+  }
+
+  return stageEvents;
+}
