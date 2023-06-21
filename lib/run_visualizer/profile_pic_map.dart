a6a0366516ebe0f54a46092c995cf15b8aa4208e
zinq7 <87585065+zinq7@users.noreply.github.com>
2023-06-21 13:17:51 -0400

    add forgotten profile pic map for comparing by nametag

---
+++ lib/run_visualizer/profile_pic_map.dart
@@ -0,0 +1,8 @@
+const MAP = <String, String>{
+  "Cabbage123": "https://yt3.googleusercontent.com/_H-mAz3ZtQ3HQ08ymGa7S4bHVcso-U0NiixDU3D2zFx_2hJiGzOSzUr6OHrs1LRR9J7diYloVA=s176-c-k-c0x00ffffff-no-rj",
+  "Nuxlar": "https://static-cdn.jtvnw.net/jtv_user_pictures/0565f80a-60e4-4cb3-a255-b69b3c30d501-profile_image-70x70.png",
+  "zinqio": "https://static-cdn.jtvnw.net/jtv_user_pictures/9d1e7994-2ebf-4617-a4f6-8263984b4b79-profile_image-70x70.png",
+  "ICap_I": "https://www.ikea.com/ca/en/images/products/knoeckla-step-trash-can-dark-gray__1029080_pe835654_s5.jpg",
+  "Kei": "https://repository-images.githubusercontent.com/412098143/e4c94bec-9e90-4be5-aa6b-1d2fa97fc420",
+  "no_pick": "https://1.bp.blogspot.com/-iCnFX7eWVjs/XR9NQutHXcI/AAAAAAAAJ9k/ISWH3UXgJF8QJdsV6P9wh3agzOwOF_aYgCLcBGAs/s1600/cat-1285634_1920.png"
+};
