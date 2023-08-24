9ec1d69711e02df5287d73315792f033ceb9f3f3
zinq7 <87585065+zinq7@users.noreply.github.com>
2023-08-23 22:29:21 -0400

    FUNCTIONAL ITEM LOOT BOX SHOWING! LFG! some issues with some images but still really cool

--- lib/map_shit/stage_view.dart
+++ lib/map_shit/stage_view.dart
@@ -4,22 +4,56 @@ import 'package:test_app/map_shit/loot_overlayer.dart';
 import 'package:test_app/run_visualizer/stage_helper.dart';
 
 /// Visualizes a run, with items from each stage on a timeline
-class StageView extends StatelessWidget {
+class StageView extends StatefulWidget {
   final String json;
   const StageView({super.key, required this.json});
 
   @override
+  State createState() => StageViewState();
+}
+
+class StageViewState extends State<StageView> {
+  int _currentStage = 0;
+  late Map json;
+
+  void changeStage(int stage) {
+    setState(() {
+      if (stage >= json["stageLoots"].length) stage = 0;
+      if (stage < 0) stage = json["stageLoots"].length - 1;
+      _currentStage = stage;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    json = jsonDecode(widget.json);
+
     // var stageEvents = getStageEvents(jsonDecode(json))[0];
-    var stageItems = getStageItems(jsonDecode(json));
-    var mapName = jsonDecode(json)["stageLoots"][0]["stageName"];
+    var stageItems = json["stageLoots"][_currentStage]["stageLoot"];
+    var mapName = json["stageLoots"][_currentStage]["stageName"];
 
     return Material(
       child: Directionality(
         textDirection: TextDirection.ltr,
-        child: LootOverlayer(
-          loot: stageItems,
-          mapName: mapName,
+        child: Scaffold(
+          body: LootOverlayer(
+            loot: stageItems,
+            mapName: mapName,
+          ),
+          bottomNavigationBar: ButtonBar(children: <Widget>[
+            TextButton(
+              child: const Text("NEXT STAGE"),
+              onPressed: () {
+                changeStage(_currentStage + 1);
+              },
+            ),
+            TextButton(
+              child: const Text("PREV STAGE"),
+              onPressed: () {
+                changeStage(_currentStage - 1);
+              },
+            ),
+          ]),
         ),
       ),
     );
