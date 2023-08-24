32920623ad240c9b8095ff78932c4acf740b2d66
zinq7 <87585065+zinq7@users.noreply.github.com>
2023-08-23 20:28:56 -0400

    untested but added stage 2s

--- lib/map_shit/map_to_image_helper.dart
+++ lib/map_shit/map_to_image_helper.dart
@@ -50,8 +50,32 @@ Map stageMap = {
   "MAP_GOLEMPLAINS_TITLE": {
     "image": "lib/assets/maps/golemplains.png",
     "ratio": (a, b, c, d) => ratioWithCorners(a, b, c, d, 465, 317, -550, -252, rotation: 90),
+  },
+  "MAP_FOGGYSWAMP_TITLE": {
+    "image": "lib/assets/maps/foggyswamp.png",
+    "ratio": (a, b, c, d) => ratioWithCorners(a, b, c, d, -460, 161, 560, -410),
+  },
+  "MAP_GOOLAKE_TITLE": {
+    "image": "lib/assets/maps/goolake_v1.png",
+    "ratio": (a, b, c, d) => ratioWithCorners(a, b, c, d, 433, 341, -432, -142, rotation: 90),
+  },
+  "MAP_ANCIENTLOFT_TITLE": {
+    "image": "lib/assets/maps/ancientloft_v2.png",
+    "ratio": (a, b, c, d) => ratioWithCorners(a, b, c, d, 432, 292, -432, -192, rotation: 90),
   }
 };
+
+// ancientloft: (90 rot)
+// topleft: 432, 292
+// bottomright: -432, -192
+
+// goodlake: (90 rot)
+// topleft: 433, 341
+// bottomright: -432,-142
+
+// foggyswamp: (0 rot)
+// topleft: -460, 161
+// bottomright: 560, -410
 
 
 // blackbeach_goodfov
